757346b8e04be08c680e6bfcaf3217b245afd90f
Daniel Ran <danielr1735@gmail.com>
2018-08-15 17:31:40 -0400

    refactor index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,42 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
   function renderSidebar(data) {
-
     let notes = data[0].notes
-
     let newHTML = notes.map(note => {
       return `<div id="note-${notes.indexOf(note)}-container" class="note-container">
       <div id="note-${notes.indexOf(note)}-preview" class="note-preview">
       <h3>${note.title}</h3>
       <p>${note.body.substr(0, 50)}...</p>
       </div>
-      </div>
-      `
-    }).join('')
-    console.log(newHTML)
+      </div>`
+      })
+      .join('')
 
     document.getElementById('sidebar').innerHTML = newHTML
   }
 
-  function renderPage(data) {
 
+const renderPage = () => {
+  fetch('http://localhost:3000/api/v1/users')
+  .then(res => res.json())
+  .then(data => {
     renderSidebar(data)
+    // renderMain(data)
+  })
+}
 
-  }
-
-fetch('http://localhost:3000/api/v1/users')
-.then(res => res.json())
-.then(data => renderPage(data))
-
-
-
-
-
-
-
-
-
-
+renderPage()
 
 })
