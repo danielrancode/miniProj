6330e37d6254ba69e858f64c9f2a0990f8cdbb88
Daniel Ran <danielr1735@gmail.com>
2018-08-15 17:10:08 -0400

    added sidebar

--- frontend/index.js
+++ frontend/index.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-  function useData(data) {
+  function renderSidebar(data) {
 
     let notes = data[0].notes
 
     let newHTML = notes.map(note => {
-      return `<div id="note-${notes.indexOf(note)}-container">
-      <div id="note-${notes.indexOf(note)}-preview">
+      return `<div id="note-${notes.indexOf(note)}-container" class="note-container">
+      <div id="note-${notes.indexOf(note)}-preview" class="note-preview">
       <h3>${note.title}</h3>
-      <h6>${note.body}</h6>
+      <p>${note.body.substr(0, 50)}...</p>
+      </div>
       </div>
       `
     }).join('')
     console.log(newHTML)
 
-    document.body.innerHTML = newHTML
-    // document.body.innerHTML = data[0].notes[0].title
+    document.getElementById('sidebar').innerHTML = newHTML
+  }
+
+  function renderPage(data) {
+
+    renderSidebar(data)
 
   }
 
 fetch('http://localhost:3000/api/v1/users')
 .then(res => res.json())
-.then(data => useData(data))
+.then(data => renderPage(data))
 
 
 
