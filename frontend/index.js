ca4e596c071d5cc56479632ccb60c2edf78976a5
Daniel Ran <danielr1735@gmail.com>
2018-08-16 10:56:02 -0400

    added funcionality

--- frontend/index.js
+++ frontend/index.js
@@ -18,11 +18,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const generateListeners = () => {
     sidebar.addEventListener('click', renderMain)
+    let createNoteSubmit = document.getElementById('create-note-submit')
+    createNoteSubmit.addEventListener('submit', (e) => {
+        e.preventDefault()
+        debugger
+    })
+
   }
 
 
   const renderSidebar = () => {
-    let newHTML = notes.map(note => {
+    // generate note previews
+    let notePreviews = notes.map(note => {
       return `<div id="note-${notes.indexOf(note)}-container" class="note-container" data-note-id="${note.id}">
       <div id="note-${notes.indexOf(note)}-preview" class="note-preview" data-note-id="${note.id}">
       <h3>${note.title}</h3>
@@ -32,7 +39,16 @@ document.addEventListener('DOMContentLoaded', () => {
       })
       .join('')
 
-    sidebar.innerHTML = newHTML
+    // create new note form
+    let newNoteButton = `<form id="create-note-form" action="index.html" method="post">
+                          <label for="new-note-title">Title:</label>
+                          <input type="text" id="new-note-title" name="new-note-title" placeholder="title">
+                          <label for="new-note-body">Body:</label>
+                          <input type="text" id="new-note-body" name="new-note-body" placeholder="body">
+                          <input id="create-note-submit" type="submit" value="Create New Note">
+                        </form>`
+    // put in html
+    sidebar.innerHTML = notePreviews + newNoteButton
   }
 
 
