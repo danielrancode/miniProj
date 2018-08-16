76da7dd59481e3b9d9c9f2556036d7fbc8ca1d3f
Daniel Ran <danielr1735@gmail.com>
2018-08-16 18:39:15 -0400

    refactor api call functions

--- frontend/index.js
+++ frontend/index.js
@@ -1,34 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const deleteNote = (e) => {
-    let noteId = e.target.parentElement.dataset.noteId
-    fetch(`http://localhost:3000/api/v1/notes/${noteId}`, {
-      method: "DELETE"
-    }).then(res => res.json())
-    .catch(error => console.error('Error...', error))
-    .then(res => console.log(res)).then(() => renderPage())
+  // API paths
+  const usersUrl = 'http://localhost:3000/api/v1/users'
+  const notesUrl = 'http://localhost:3000/api/v1/notes'
+  const noteUrl = id => `${notesUrl}/${id}`
 
-  }
+  // HTML elements
+  const sidebar =  document.getElementById('sidebar')
+  const mainContent = document.getElementById('main-content')
 
   const getNote = (id, callback) => {
     let note
-    return fetch(`http://localhost:3000/api/v1/notes/${id}`)
+    return fetch(noteUrl(id))
     .then(res => res.json())
-    .catch(error => console.error('Error!!!', error))
+    .catch(error => console.error('Error', error))
     .then(data => {
-    console.log('Success!!!')
     return callback(data)
   })
   }
 
+  const callApi = (call) => {
+    return call.then(res => res.json())
+    .catch(error => console.error('Error', error))
+    .then(res => console.log('Success', res)).then(() => renderPage())
+  }
+
+  const post = (note) => {
+    return fetch(notesUrl, {
+      method: "POST",
+      body: JSON.stringify(note),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  const update = (note) => {
+    return fetch(noteUrl(note.id), {
+      method: "PATCH",
+      body: JSON.stringify(note),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  const deleteNote = (noteId) => {
+    return fetch(noteUrl(noteId), {
+      method: "DELETE"
+    })
+  }
+
   const renderEditNoteForm = (note) => {
-    console.log(note.id)
-    // console.log(note.title)
-    // console.log(note.body)
-    // console.log(note.user.name)
-    // console.log(note.user.id)
     let mainContent = document.getElementById('main-content')
-    mainContent.innerHTML = `<form id="edit-note-form" action="http://localhost:3000/api/v1/notes/${note.id}">
+    mainContent.innerHTML = `<form id="edit-note-form" action="${noteUrl(note.id)}">
                           <label for="edit-note-title">Title:</label>
                           <input type="text" id="edit-note-title" name="edit-note-title" value="${note.title}">
                           <label for="edit-note-body">Body:</label>
@@ -41,36 +66,32 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault()
         const noteTitle = document.getElementById('edit-note-title').value
         const noteBody = document.getElementById('edit-note-body').value
-        updateNote({id: note.id, title: noteTitle, body: noteBody, user_id:1})
+        callApi(update({id: note.id, title: noteTitle, body: noteBody, user_id:1}))
     })
   }
 
     const renderMain = (e = null) => {
-      let mainContent = document.getElementById('main-content')
       if (e == null) {
         mainContent.innerHTML = ''
       } else if (e.target.className == 'edit-note-button') {
         let noteId = e.target.parentElement.dataset.noteId
-        console.log(noteId)
-        // mainContent.innerHTML =
         getNote(noteId, renderEditNoteForm)
+      } else if (e.target.className == 'delete-note-button') {
+        let noteId = e.target.parentElement.dataset.noteId
+        console.log(e.target.parentElement.dataset)
+        getNote(noteId, callApi(deleteNote(noteId)))
+        // document.getElementById('delete-note-button').addEventListener('click', deleteNote)
       } else {
         let noteId = e.target.parentElement.dataset.noteId
         let note = notes.find(note => note.id == noteId)
         let noteContent =
         `<div data-note-id="${note.id}"> <h3>${note.title}</h3>
         <p>${note.body}</p>
-        <button id="delete-note-button" type="button">Delete Note</button>
         </div>
         `
         mainContent.innerHTML = noteContent
-        document.getElementById('delete-note-button').addEventListener('click', deleteNote)
       }
     }
-
-
-  const sidebar =  document.getElementById('sidebar')
-
 
   const generateListeners = () => {
     sidebar.addEventListener('click', renderMain)
@@ -79,7 +100,8 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault()
         const newNoteTitle = document.getElementById('new-note-title').value
         const newNoteBody = document.getElementById('new-note-body').value
-        postNote({title: newNoteTitle, body: newNoteBody, user_id:1})
+        callApi(post({title: newNoteTitle, body: newNoteBody, user_id:1}))
+        // postNote({title: newNoteTitle, body: newNoteBody, user_id:1})
     })
   }
 
@@ -92,13 +114,14 @@ document.addEventListener('DOMContentLoaded', () => {
       <h3>${note.title}</h3>
       <p>${note.body.substr(0, 50)}...</p>
       <button class="edit-note-button" type="button">Edit</button>
+      <button class="delete-note-button" type="button">Delete</button>
       </div>
       </div>`
       })
       .join('')
 
     // create new note form
-    let createNoteForm = `<form id="create-note-form" action="http://localhost:3000/api/v1/notes">
+    let createNoteForm = `<form id="create-note-form" action="${notesUrl}">
                           <label for="new-note-title">Title:</label>
                           <input type="text" id="new-note-title" name="new-note-title" placeholder="title">
                           <label for="new-note-body">Body:</label>
@@ -109,34 +132,8 @@ document.addEventListener('DOMContentLoaded', () => {
     sidebar.innerHTML = notePreviews + createNoteForm
   }
 
-  const postNote = (noteObj) => {
-    return fetch('http://localhost:3000/api/v1/notes', {
-      method: "POST",
-      body: JSON.stringify(noteObj),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-    .catch(error => console.error('Error!!!', error))
-    .then(res => console.log('Success!!!', res)).then(() => renderPage())
-
-  }
-  const updateNote = (noteObj) => {
-    return fetch(`http://localhost:3000/api/v1/notes/${noteObj.id}`, {
-      method: "PATCH",
-      body: JSON.stringify(noteObj),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-    .catch(error => console.error('Error!!!', error))
-    .then(res => console.log('Success!!!', res)).then(() => renderPage())
-
-  }
-
-
   const renderPage = () => {
-    fetch('http://localhost:3000/api/v1/users')
+    fetch(usersUrl)
     .then(res => res.json())
     .then(data => {
       notes = data[0].notes
@@ -146,7 +143,6 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 }
 
-let noteGlobal
 let notes
 renderPage()
 
