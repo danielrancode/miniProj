f8a99fc86fb5e530b3e273c9737726edbd41ad92
Daniel Ran <danielr1735@gmail.com>
2018-08-17 01:23:24 -0400

    major refactoring

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+
   // API paths
   const usersUrl = 'http://localhost:3000/api/v1/users'
   const notesUrl = 'http://localhost:3000/api/v1/notes'
@@ -7,51 +9,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // HTML elements
   const sidebar =  document.getElementById('sidebar')
+  const notePreviewsDisplay = document.getElementById('note-previews')
   const mainContent = document.getElementById('main-content')
 
-  const getNote = (id, callback) => {
-    let note
-    return fetch(noteUrl(id))
-    .then(res => res.json())
-    .catch(error => console.error('Error', error))
-    .then(data => {
-    return callback(data)
-  })
+
+  function renderPage() {
+    fetchAllNotes(displayNotePreviews)
+    mainDisplay()
+    generateListeners()
   }
 
-  const callApi = (call) => {
-    return call.then(res => res.json())
-    .catch(error => console.error('Error', error))
-    .then(res => console.log('Success', res)).then(() => renderPage())
+  function fetchAllNotes(callback) {
+    fetch(notesUrl).then(res => res.json()).then(callback)
   }
 
-  const post = (note) => {
-    return fetch(notesUrl, {
-      method: "POST",
-      body: JSON.stringify(note),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+  function displayNotePreviews(notes) {
+    notePreviewsDisplay.innerHTML = notes.reverse().map(note =>
+      `<div id="note-${notes.indexOf(note)}-container" class="note-container" data-note-id="${note.id}">
+          <div id="note-${notes.indexOf(note)}-preview" class="note-preview" data-note-id="${note.id}">
+            <h3>${note.title}</h3>
+            <p>${note.body.substr(0, 50)}...</p>
+            <button class="edit-note-button" type="button">Edit</button>
+            <button class="delete-note-button" type="button">Delete</button>
+          </div>
+        </div>`)
+      .join('')
   }
 
-  const update = (note) => {
-    return fetch(noteUrl(note.id), {
-      method: "PATCH",
-      body: JSON.stringify(note),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+
+  function mainDisplay(e) {
+    if (!e) {
+      mainContent.innerHTML = ''
+    } else {
+        let id = e.target.parentElement.dataset.noteId
+        if (e.target.className == 'edit-note-button') {
+            getNote(id, renderEditNoteForm)
+        } else if (e.target.className == 'delete-note-button') {
+            getNote(id, callApi(deleteNote(id)))
+        } else {
+            getNote(id, (note) => {
+              mainContent.innerHTML = `
+              <div data-note-id="${note.id}">
+                <h3>${note.title}</h3>
+                <p>${note.body}</p>
+              </div>
+              `})
+        }
+    }
   }
 
-  const deleteNote = (noteId) => {
-    return fetch(noteUrl(noteId), {
-      method: "DELETE"
-    })
+
+  function getNote(id, callback) {
+    return fetch(`${notesUrl}/${id}`).then(res => res.json()).catch(error => console.error('Error', error)).then(callback)
   }
 
-  const renderEditNoteForm = (note) => {
+  function callApi(call) {
+    return call.then(res => res.json()).catch(error => console.error('Error', error)).then(res => console.log('Success', res)).then(() => renderPage())
+  }
+
+  function post(note) {
+    return fetch(notesUrl, {method: "POST", body: JSON.stringify(note), headers: {'Content-Type': 'application/json'}})
+  }
+
+  function update(note) {
+    return fetch(`${notesUrl}/${note.id}`, {method: "PATCH", body: JSON.stringify(note), headers: {'Content-Type': 'application/json'}})
+  }
+
+  function deleteNote(id) {
+    return fetch(`${notesUrl}/${id}`, {method: "DELETE"})
+  }
+
+  function renderEditNoteForm(note) {
     let mainContent = document.getElementById('main-content')
     mainContent.innerHTML = `<form id="edit-note-form" action="${noteUrl(note.id)}">
                           <label for="edit-note-title">Title:</label>
@@ -70,31 +98,10 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-    const renderMain = (e = null) => {
-      if (e == null) {
-        mainContent.innerHTML = ''
-      } else if (e.target.className == 'edit-note-button') {
-        let noteId = e.target.parentElement.dataset.noteId
-        getNote(noteId, renderEditNoteForm)
-      } else if (e.target.className == 'delete-note-button') {
-        let noteId = e.target.parentElement.dataset.noteId
-        console.log(e.target.parentElement.dataset)
-        getNote(noteId, callApi(deleteNote(noteId)))
-        // document.getElementById('delete-note-button').addEventListener('click', deleteNote)
-      } else {
-        let noteId = e.target.parentElement.dataset.noteId
-        let note = notes.find(note => note.id == noteId)
-        let noteContent =
-        `<div data-note-id="${note.id}"> <h3>${note.title}</h3>
-        <p>${note.body}</p>
-        </div>
-        `
-        mainContent.innerHTML = noteContent
-      }
-    }
+
 
   const generateListeners = () => {
-    sidebar.addEventListener('click', renderMain)
+    sidebar.addEventListener('click', mainDisplay)
     let createNoteForm = document.getElementById('create-note-form')
     createNoteForm.addEventListener('submit', (e) => {
         e.preventDefault()
@@ -105,45 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  renderPage()
 
-  const renderSidebar = () => {
-    // generate note previews
-    let notePreviews = notes.map(note => {
-      return `<div id="note-${notes.indexOf(note)}-container" class="note-container" data-note-id="${note.id}">
-      <div id="note-${notes.indexOf(note)}-preview" class="note-preview" data-note-id="${note.id}">
-      <h3>${note.title}</h3>
-      <p>${note.body.substr(0, 50)}...</p>
-      <button class="edit-note-button" type="button">Edit</button>
-      <button class="delete-note-button" type="button">Delete</button>
-      </div>
-      </div>`
-      })
-      .join('')
-
-    // create new note form
-    let createNoteForm = `<form id="create-note-form" action="${notesUrl}">
-                          <label for="new-note-title">Title:</label>
-                          <input type="text" id="new-note-title" name="new-note-title" placeholder="title">
-                          <label for="new-note-body">Body:</label>
-                          <input type="text" id="new-note-body" name="new-note-body" placeholder="body">
-                          <input id="create-note-submit" type="submit" value="Create New Note">
-                        </form>`
-    // put in html
-    sidebar.innerHTML = notePreviews + createNoteForm
-  }
-
-  const renderPage = () => {
-    fetch(usersUrl)
-    .then(res => res.json())
-    .then(data => {
-      notes = data[0].notes
-    renderSidebar()
-    renderMain()
-    generateListeners()
-  })
-}
-
-let notes
-renderPage()
 
 })
